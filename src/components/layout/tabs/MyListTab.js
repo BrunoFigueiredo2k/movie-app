02c096ee02854f3c2ab1228afde55afbdb9ce88c
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2021-01-03 13:55:22 +0100

    if there are no movies in list let the user know

--- src/components/layout/tabs/MyListTab.js
+++ src/components/layout/tabs/MyListTab.js
@@ -35,47 +35,54 @@ export default function MyListTab() {
     return (
         <div className="container">
             <h1 className="heading-page">My List</h1>
-            <Tabs
-            defaultActiveKey="All"
-            id="controlled-tab-example"
-            activeKey={key}
-            onSelect={(k) => setKey(k)}>
-                <Tab eventKey="All" title="All Movies" />
-                {watchStatus.map(status => {
-                    return <Tab eventKey={status} title={status} />
-                })}
-            </Tabs>
-            <table className="table-my-list">
-                <thead>
-                <tr>
-                    <th style={{width: '5px'}}></th>
-                    <th style={{width: '20px', textAlign: 'center'}}>#</th>
-                    <th style={{width: '70px', textAlign: 'center'}}>Image</th>
-                    <th>Title</th>
-                    <th style={{width: '120px'}}>Date added</th>
-                    <th style={{width: '100px',  textAlign: 'center'}}>Score</th>
-                </tr>
-                </thead>
-                {myMovies.map((movie, index) => {
-                    // If movie status is equal to current key that is set by tab, then display. Also display everything for key 'All movies'
-                    if (movie.userStats.status == key || key == "All"){
-                        return (
-                            <tbody>
-                            <tr>
-                                <td style={{backgroundColor: determineColorStatus(movie.userStats.status)}}></td>
-                                <td style={{color: '#ffd1df', textAlign: 'center', fontWeight: 'bold'}}>{index + 1}</td>
-                                <td><img src={IMG_BASE_URL + movie.movie.movieItem.poster_path} width={70} /></td>
-                                <td style={{fontWeight: 'bold', fontSize: '22px', letterSpacing: '1px'}}>{movie.movie.movieItem.original_title}</td>
-                                <td style={{fontSize: '16px'}}>{movie.date}</td>
-                                <td style={{fontWeight: 'bold', fontSize: '22px', textAlign: 'center'}}>
-                                    {showRatingNumber(movie.userStats.rating)}
-                                </td>
-                            </tr>
-                            </tbody>
-                        )
-                    }
-                })}
-            </table>
+            {myMovies.length ? // If length of movies array is not 0 then return table of movies
+            <>
+                <Tabs
+                defaultActiveKey="All"
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}>
+                    <Tab eventKey="All" title="All Movies" />
+                    {watchStatus.map(status => {
+                        return <Tab eventKey={status} title={status} />
+                    })}
+                </Tabs>
+                <table className="table-my-list">
+                    <thead>
+                    <tr>
+                        <th style={{width: '5px'}}></th>
+                        <th style={{width: '20px', textAlign: 'center'}}>#</th>
+                        <th style={{width: '70px', textAlign: 'center'}}>Image</th>
+                        <th>Title</th>
+                        <th style={{width: '120px'}}>Date added</th>
+                        <th style={{width: '100px',  textAlign: 'center'}}>Score</th>
+                    </tr>
+                    </thead>
+                    {myMovies.map((movie, index) => {
+                        // If movie status is equal to current key that is set by tab, then display. Also display everything for key 'All movies'
+                        if (movie.userStats.status == key || key == "All"){
+                            return (
+                                <tbody>
+                                <tr>
+                                    <td style={{backgroundColor: determineColorStatus(movie.userStats.status)}}></td>
+                                    <td style={{color: '#ffd1df', textAlign: 'center', fontWeight: 'bold'}}>{index + 1}</td>
+                                    <td><img src={IMG_BASE_URL + movie.movie.movieItem.poster_path} width={70} /></td>
+                                    <td style={{fontWeight: 'bold', fontSize: '22px', letterSpacing: '1px'}}>{movie.movie.movieItem.original_title}</td>
+                                    <td style={{fontSize: '16px'}}>{movie.date}</td>
+                                    <td style={{fontWeight: 'bold', fontSize: '22px', textAlign: 'center'}}>
+                                        {showRatingNumber(movie.userStats.rating)}
+                                    </td>
+                                </tr>
+                                </tbody>
+                            )
+                        }
+                    })}
+                </table>
+                </>
+            : <>
+            <p style={{color: 'white'}}>No movies in your list yet...</p><br/>
+            <img src="https://i.imgur.com/sUpua6W.gif"/>
+            </>}
         </div>
     )
 }

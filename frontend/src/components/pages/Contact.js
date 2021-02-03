0d62437b8128a0ce5142269c71fde2549d549d5a
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2021-02-03 23:17:04 +0100

    reset input fields and success message form

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,49 +1,74 @@
-import React, {useState, useCallback, useEffect} from 'react'
+import React, {useCallback, useState, useEffect, useRef} from 'react'
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
 import {getForms, postForm} from '../../services/Api'
+import ConfirmationMessage from '../../components/layout/ConfirmationMessage';
+import {checkIfDisplayMesssageChanged} from '../utils';
 
 export default function Contact() {
-    const [inputFields, setInputFields] = useState({fullName: '', email: '', message: ''})
+    const [displayMessage, setDisplayMessage] = useState({
+        display: false,
+        type: '',
+        content: ''
+    });
+
+    let fullNameVal = useRef('');
+    let emailVal = useRef('');
+    let messageVal = useRef('');
 
     const submitFormDetails = useCallback(
         async event => {
             event.preventDefault();
             const {fullName, email, message} = event.target.elements;
             try {
-                // set values in state object
-                setInputFields({ fullName: fullName.value, email: email.value, message: message.value });
+                // Save values in state
+                const inputFields = { fullName: fullName.value, email: email.value, message: message.value };
+                postForm(inputFields)
+                    .then(res => {
+                        // reset input fields when submitted
+                        fullNameVal.current.value = '';
+                        emailVal.current.value = '';
+                        messageVal.current.value = '';
+
+                        // Set succes message
+                        setDisplayMessage({
+                            display: true,
+                            type: 'success',
+                            content: 'Thank you very much! Your submission has been received.'
+                        });
+                    })
+                    .catch(err => {
+                        alert(err);
+                    })
             } catch (error) {
                 alert(error);
             }
         }
     );
 
-    // When the input fields are updated post data to api
-    useEffect(() => {
-        postForm(inputFields);
-    }, [inputFields])
+    checkIfDisplayMesssageChanged(displayMessage, setDisplayMessage, 3000);
 
     return (
         <div>
             <div className="section">
                 <Header/>
                 <div className="container mt-5">
+                    {displayMessage.display ? <ConfirmationMessage type={displayMessage.type} message={displayMessage.content} /> : null}
                     <div className="row">
                         <div className="card bg-light col-md-6 offset-md-3 offset-md-3">
                             <h2 className="text-center mt-3 font-weight-bold">Contact us</h2>
                             <form className="card-body" onSubmit={submitFormDetails}>
                                 <div className="form-group">
                                     <p>Full Name:</p>
-                                    <input type="text" name="fullName" placeholder="Your full name" class="form-control" required/>
+                                    <input type="text" ref={fullNameVal} name="fullName" placeholder="Your full name" className="form-control" required/>
                                 </div>
                                 <div className="form-group">
                                     <p>Email:</p>
-                                    <input type="email" name="email" placeholder="Your email address" class="form-control" required/>
+                                    <input type="email" ref={emailVal} name="email" placeholder="Your email address" className="form-control" required/>
                                 </div>
                                 <div className="form-group">
                                     <p>Message:</p>
-                                    <textarea type="text" name="message" placeholder="Write your message here.." class="form-control" required/>
+                                    <textarea type="text" ref={messageVal} name="message" placeholder="Write your message here.." className="form-control" required/>
                                 </div>
                                 <div>
                                     <button className="btn btn-primary" rows={6} style={{width: '100%'}} type="submit">Submit</button>
